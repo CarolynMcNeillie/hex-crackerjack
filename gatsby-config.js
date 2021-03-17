f49195379c2174a7cf274fbbdf990dddbbb11001
Carolyn McNeillie <cmcneillie@theworkinggroup.ca>
2021-03-17 13:05:30 -0400

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  siteMetadata: {
+    title: "Hex Crackerjack",
+  },
+  plugins: [
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-gatsby-cloud",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+  ],
+};
